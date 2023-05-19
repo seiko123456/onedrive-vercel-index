2f00cc16b8e31f9db969642fc15c7c4ed7aaf61a
andy <45893673+seiko123456@users.noreply.github.com>
2023-05-19 18:06:07 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -11,16 +11,16 @@ module.exports = {
 
   // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
   // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
-  icon: '/icons/128.png',
+  icon: 'https://i.imgur.com/qgg6tvg.png',
 
   // Prefix for KV Storage
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "OneDrive",
+  title: "cs",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
-  baseDirectory: '/Public',
+  baseDirectory: '/CZ/',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
